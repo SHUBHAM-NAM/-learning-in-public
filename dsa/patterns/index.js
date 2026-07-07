be04547f0af21785a13126d952ef0bef445497fc
Shubham choudhary <choudharyshubham22105@gmail.com>
2026-07-07 21:12:08 +0530

    add README to document journey in backend development and DSA

---
+++ dsa/patterns/index.js
@@ -0,0 +1,20 @@
+
+// star printing
+const prompt = require('prompt-sync')();
+let n=Number(prompt("enter the rows"))
+// let m=Number(prompt("enter the col"))
+
+
+// print an centred payramind pattern
+// for(let i=1; i<=n; i++){
+//     let line="";
+//     for(let k=1; k<=n-i; k++){
+//         line=line+" "
+//     }
+//     for(let j=1; j<=i; j++){
+//         line=line+'* '
+// }
+// console.log(line);
+// }
+// print an centred reverse payramind pattern
+for(let i=1)
